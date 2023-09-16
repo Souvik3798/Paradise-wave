20233e2288210ae7a43ae9f1ec827f99061dce3f
Souvik3798 <souvikbarua693@gmail.com>
2023-09-16 20:26:19 +0530

    order removed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -206,7 +206,7 @@
                 <div class="col-xl-12">
                     <div class="testmonial_active owl-carousel">
                         @php
-                            $posts = \App\Models\Posts::all()->orderByDesc('id')->take(8);
+                            $posts = \App\Models\Posts::all()->take(8);
                         @endphp
                         @foreach ($posts as $post)
                             <div class="single_carousel">
